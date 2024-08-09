6dc2972393e2c08d04a35af01a7b68afdca1143e
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-08 19:59:16 -0500

    Completed the encrypt function in Adv mode.

--- main.py
+++ main.py
@@ -405,13 +405,179 @@ def adv_index():
                 # final = jk
                 pass
             else:
-                # x = make_short(encryt(user_input_list,key))
-                # final = x
+                x = make_short(encryt(user_input_list,key))
+                final = x
                 pass
         def text_to_binary(s):
             return ' '.join(format(ord(c), '08b') for c in s)
         def binary_to_text(s):
             return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
+        def encryt(text_in_binary_list, key):
+            global encryted_text_in_binary
+            global encryted_text_in_binary_list
+            # Io = ["1", "0"]
+            # user_nums = []
+            # for i in range(12):
+            #     f = ""
+            #     for _ in range(8):
+            #         f = f+random.choice(Io)
+            #     user_nums.append(f)
+            # user_nums_string = ""
+            # for i in user_nums:
+            #     user_nums_string += i
+            encryted_text_in_binary_list = []
+            for i in text_in_binary_list:
+                user_final = i#+user_nums_string
+                for x in key:
+                    encryted_text_in_binary_list.append(user_final[x-1])
+                encryted_text_in_binary = ""
+                for i in encryted_text_in_binary_list:
+                    encryted_text_in_binary += i
+            return encryted_text_in_binary
+        def make_short(bin):
+            c = 0
+            temp = ""
+            z = []
+            for i in bin:
+                c += 1
+                if len(temp) == 2:
+                    z.append(temp)
+                    temp = ""
+                    temp += str(i)
+                else:
+                    temp += str(i)
+                    if len(temp) == 2:
+                        z.append(temp)
+                        temp = ""
+            indices_10 = [i for i, x in enumerate(z) if x == "10"]
+            for i in indices_10:
+                z[i] = "a"
+            indices_01 = [i for i, x in enumerate(z) if x == "01"]
+            for i in indices_01:
+                z[i] = "b"
+            indices_11 = [i for i, x in enumerate(z) if x == "11"]
+            for i in indices_11:
+                z[i] = "c" 
+            indices_00 = [i for i, x in enumerate(z) if x == "00"]
+            for i in indices_00:
+                z[i] = "d"
+            short_1_bin = ""
+            for i in z:
+                short_1_bin += i
+            c = 0
+            temp = ""
+            z_2 = []
+            for i in z:
+                c += 1
+                if len(temp) == 2:
+                    z_2.append(temp)
+                    temp = ""
+                    temp += str(i)
+                else:
+                    temp += str(i)
+                    if len(temp) == 2:
+                        z_2.append(temp)
+                        temp = ""
+            z = z_2
+            indices_ab = [i for i, x in enumerate(z) if x == "ab"]
+            for i in indices_ab:
+                try:
+                    z_2[i] = "q"
+                except:
+                    pass
+            indices_ac = [i for i, x in enumerate(z) if x == "ac"]
+            for i in indices_ac:
+                try:
+                    z_2[i] = "w"
+                except:
+                    pass
+            indices_ad = [i for i, x in enumerate(z) if x == "ad"]
+            for i in indices_ad:
+                try:
+                    z_2[i] = "!"
+                except:
+                    pass
+            indices_ba = [i for i, x in enumerate(z) if x == "ba"]
+            for i in indices_ba:
+                try:
+                    z_2[i] = "e"
+                except:
+                    pass 
+            indices_bc = [i for i, x in enumerate(z) if x == "bc"]
+            for i in indices_bc:
+                try:
+                    z_2[i] = "r"
+                except:
+                    pass
+            indices_bd = [i for i, x in enumerate(z) if x == "bd"]
+            for i in indices_bd:
+                try:
+                    z_2[i] = "g"
+                except:
+                    pass
+            indices_ca = [i for i, x in enumerate(z) if x == "ca"]
+            for i in indices_ca:
+                try:
+                    z_2[i] = "t"
+                except:
+                    pass
+            indices_cb = [i for i, x in enumerate(z) if x == "cb"]
+            for i in indices_cb:
+                try:
+                    z_2[i] = "%"
+                except:
+                    pass
+            indices_cd = [i for i, x in enumerate(z) if x == "cd"]
+            for i in indices_cd:
+                try:
+                    z_2[i] = "&"
+                except:
+                    pass
+            indices_aa = [i for i, x in enumerate(z) if x == "aa"]
+            for i in indices_aa:
+                try:
+                    z_2[i] = "@"
+                except:
+                    pass
+            indices_bb = [i for i, x in enumerate(z) if x == "bb"]
+            for i in indices_bb:
+                try:
+                    z_2[i] = "#"
+                except:
+                    pass
+            indices_cc = [i for i, x in enumerate(z) if x == "cc"]
+            for i in indices_cc:
+                try:
+                    z_2[i] = "o"
+                except:
+                    pass
+            indices_dc = [i for i, x in enumerate(z) if x == "dc"]
+            for i in indices_dc:
+                try:
+                    z_2[i] = "j"
+                except:
+                    pass
+            indices_db = [i for i, x in enumerate(z) if x == "db"]
+            for i in indices_db:
+                try:
+                    z_2[i] = "k"
+                except:
+                    pass
+            indices_da = [i for i, x in enumerate(z) if x == "da"]
+            for i in indices_da:
+                try:
+                    z_2[i] = "l"
+                except:
+                    pass
+            # adding identity letters
+            z.reverse()
+            z.append("--9")
+            z.reverse()
+            z.append("9--")
+            short_2_bin = ""
+            for i in z_2:
+                short_2_bin += i
+            return short_2_bin
 
 if __name__ == "__main__":
     app.run()
